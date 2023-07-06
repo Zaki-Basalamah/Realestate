19f1191c9687f4dd42c8eb990c379bdba3aabd84
Zaki Basalamah <zacy.n89@gmail.com>
2023-07-06 16:19:58 +0700

    Update Featured.js

--- src/components/featured/Featured.js
+++ src/components/featured/Featured.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import './Featured.css'
 
-import House1 from '../../assets/house1.jpg'
+import House1 from '../../assets/house1.jpeg'
 import Bed1 from '../../assets/bed1.jpg'
 import Bed2 from '../../assets/bed2.jpg'
 import Kitchen from '../../assets/kitchen.jpg'
@@ -17,7 +17,7 @@ import LivingRoom from '../../assets/living-room.jpg'
 const Featured = () => {
     return (
         <div className='featured'>
-            <h1 className='featured-text'>Top Featured Listings</h1>
+            <h1 className='featured-text'>Daftar villa dan rumah</h1>
             <p className='featured-text'>Selected listings by City, State, & Zip based on views.</p>
             <div className='container'>
                 <img className='span-3 image-grid-row-2' src={House1} alt='' />
@@ -27,31 +27,31 @@ const Featured = () => {
                 <img src={Bathroom} alt='' />
                 <div className='span-3 img-details'>
                     <div className='top'>
-                        <h2>123 Acme St. Dallas, TX</h2>
+                        <h2>Teluk Bayur Utama Residence</h2>
                         <p>House for Sale</p>
-                        <p className='price'>$2,677,000</p>
+                        <p className='price'>Rp 352.000.000</p>
                     </div>
                     <div className='info-grid'>
                         <div>
                             <div className='info'>
-                                <p className='bold'>Bedrooms:</p><p>5</p>
+                                <p className='bold'>Bedrooms:</p><p>2</p>
                             </div>
                             <div className='info'>
-                                <p className='bold'>Bathrooms:</p><p>7</p>
+                                <p className='bold'>Bathrooms:</p><p>1</p>
                             </div>
                         </div>
                         <div>
                             <div className='info'>
-                                <p className='bold'>Square Feet:</p><p>8,138</p>
+                                <p className='bold'>m2:</p><p>60</p>
                             </div>
                             <div className='info'>
-                                <p className='bold'>Est Payment:</p><p>$14,797/mo</p>
+                                <p className='bold'>:</p><p></p>
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className='span-2 right-img-details'>
-                    <p>A beautiful modern day home in the city with a full-size pool. Spacious and luxurious home located in Texas. Including media room, workout facility, and built-in sauna. </p>
+                    <p>Jl.Simpang Teluk Bayur gang 10 ,Malang City- East Java,Indonesia ,65124. Hunian super stategis di kota malang. A beautiful modern day home in the city. Spacious and luxurious home located in Malang.</p>
                     <button className='btn'>View Listing</button>
                 </div>
             </div>
@@ -68,32 +68,32 @@ const Featured = () => {
                 <img className='order-5' src={LivingRoom} alt='' />
 
                 <div className='span-2 right-img-details order-7'>
-                    <p>A beautiful modern day home in the city with a full-size pool. Spacious and luxurious home located in Texas. Including media room, workout facility, and built-in sauna. </p>
+                    <p>A beautiful modern day home in the city. Spacious and luxurious villa located in Malang. Including and built-in sauna. </p>
                     <button className='btn'>View Listing</button>
                 </div>
 
 
                 <div className='span-3 img-details order-6'>
                     <div className='top'>
-                        <h2>123 Acme St. Dallas, TX</h2>
-                        <p>House for Sale</p>
-                        <p className='price'>$2,677,000</p>
+                        <h2>Alibaba Village </h2>
+                        <p>Villa for Sale</p>
+                        <p className='price'>Rp 749.000.000</p>
                     </div>
                     <div className='info-grid'>
                         <div>
                             <div className='info'>
-                                <p className='bold'>Bedrooms:</p><p>5</p>
+                                <p className='bold'>Bedrooms:</p><p>4</p>
                             </div>
                             <div className='info'>
-                                <p className='bold'>Bathrooms:</p><p>7</p>
+                                <p className='bold'>Bathrooms:</p><p>2</p>
                             </div>
                         </div>
                         <div>
                             <div className='info'>
-                                <p className='bold'>Square Feet:</p><p>8,138</p>
+                                <p className='bold'>m2</p><p>70 m2</p>
                             </div>
                             <div className='info'>
-                                <p className='bold'>Est Payment:</p><p>$14,797/mo</p>
+                                <p className='bold'>:</p><p></p>
                             </div>
                         </div>
                     </div>
